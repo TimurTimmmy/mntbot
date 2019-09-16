c9a937bb3fbcac41c908a07e86ee19b2518d31a5
Timur Timmmy <ash123@mail.ru>
2019-09-16 16:15:50 +0300

    up

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 const Telegraf = require('telegraf')
+var BotTocken = '555289887:AAGlrPpG8Opm6sgxXKITGDQ4xSlGQKU96nA'
 
-const bot = new Telegraf(process.env.555289887:AAGlrPpG8Opm6sgxXKITGDQ4xSlGQKU96nA)
+const bot = new Telegraf(BotTocken)
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply(''))
